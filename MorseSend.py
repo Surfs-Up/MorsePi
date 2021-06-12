0eac4df174ae7e8759130d0d087bce97d8ec2b8a
Nithin <mr.picklepinosaur@gmail.com>
2021-06-12 16:50:18 +0000

    client start

--- MorseSend.py
+++ MorseSend.py
@@ -1,6 +1,7 @@
 import RPi.GPIO as GPIO
 
 from time import sleep
+import time
 GPIO.setwarnings(False)
 # GPIO.setmode(GPIO.BOARD)
 # GPIO.setup(8,GPIO.OUT,initial = GPIO.LOW)
@@ -11,44 +12,38 @@ GPIO.setwarnings(False)
 #     GPIO.output(8, GPIO.LOW)  # Turn off
 #     sleep(1)                  # Sleep for 1 second
 class MorseEncoder:
-    clk = 11
-    dt = 12
-    btn = 10
+    btn = 7
     counter = 0
-    clkLastState = 0
     message = ""
+    
     def __init__(self):
 
         GPIO.setmode(GPIO.BOARD)
-        GPIO.setup(MorseEncoder.clk, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(MorseEncoder.dt, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-        GPIO.setup(MorseEncoder.btn,GPIO.IN,pull_up_down=GPIO.PUD_DOWN)
-        GPIO.add_event_detect(MorseEncoder.btn,GPIO.RISING,callback=self.click,bouncetime=20)
+        GPIO.setup(MorseEncoder.btn,GPIO.IN)
+        GPIO.add_event_detect(MorseEncoder.btn,GPIO.BOTH,callback=self.click,bouncetime=20)
         self.counter = 0
-        self.clkLastState = GPIO.input(MorseEncoder.clk)
         self.message = ""
-        self.detect_rotary()
+        self.btn_start = 0
+
+        self.run()
+
+        
         
     
     def click(self,channel):
-
-        print("click")
-    def detect_rotary(self):
-        try:
-            while True:
-                clkState = GPIO.input(MorseEncoder.clk)
-                dtState = GPIO.input(MorseEncoder.dt)
-                if clkState != self.clkLastState:
-                    if dtState!=clkState:
-                        self.counter+=1
-                    else: self.counter-=1
-                    print(self.counter)
-                self.clkLastState = clkState
-                sleep(0.01)
-        finally:
-            GPIO.cleanup()
+        if GPIO.input(self.btn)==0:
+            self.btn_start = time.time()
+        else:
+            end = time.time()
+            elapsed = end-self.btn_start
+            print(elapsed)
+    
     def send_message(self):
         pass
+    def run(self):
+        while True:
+            pass
+    
 
         
         
